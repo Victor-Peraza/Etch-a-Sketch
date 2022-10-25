5e41f7ce89915f389cd75e22fc0e4f43bab3330b
Victor Peraza <perazav@outlook.com>
2022-10-25 16:49:14 -0700

    Added drawing feature

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
-const container = document.querySelector('#container');
-let squares =16;
+const container = document.querySelector("#container");
+let squares = 16;
 let dimensions = squares * squares;
 let padding = 1;
+const makeBlack = (event) => (event.target.style.backgroundColor = "black");
 for (let i = 0; i < dimensions; i++) {
-    const gridSquares = document.createElement('div');
-    container.style = 'grid-template-columns: repeat('+squares+',' + padding + 'fr);grid-template-rows: repeat('+squares+',' + padding + 'fr);'
-    gridSquares.classList.add('boxes');
-    container.appendChild(gridSquares);
+  const gridSquares = document.createElement("div");
+  container.style =
+    "grid-template-columns: repeat(" +
+    squares +
+    "," +
+    padding +
+    "fr);grid-template-rows: repeat(" +
+    squares +
+    "," +
+    padding +
+    "fr);";
+
+  gridSquares.classList.add("boxes");
+  container.appendChild(gridSquares);
+
+  //one click and paint
+  gridSquares.addEventListener("mousedown", (event) => {
+    if (event.buttons == 1) {
+      makeBlack(event);
+    }
+  });
+
+  //click and hold/drag mouse then paint
+  gridSquares.addEventListener("mouseenter", (event) => {
+    if (event.buttons == 1) {
+      makeBlack(event);
+    }
+  });
 }
-
-
-
-
-
-
-
