b1ae41694b278a77f9f68d1742b58bedae8f4036
Victor Peraza <perazav@outlook.com>
2022-11-08 18:39:09 -0800

    added erase and clear functionality

--- script.js
+++ script.js
@@ -2,9 +2,17 @@ const container = document.querySelector("#container");
 let squares = 16;
 let dimensions = squares * squares;
 let padding = 1;
+
+const draw = document.querySelector("#draw");
+const eraser = document.querySelector("#eraser");
+const clear = document.querySelector("#clear");
+
 const makeBlack = (event) => (event.target.style.backgroundColor = "black");
+const makeErase = (event) => (event.target.style.backgroundColor = "#F8F8F8");
+const makeClear = (event) => (event.style.backgroundColor = "#F8F8F8")
 for (let i = 0; i < dimensions; i++) {
   const gridSquares = document.createElement("div");
+
   container.style =
     "grid-template-columns: repeat(" +
     squares +
@@ -18,18 +26,55 @@ for (let i = 0; i < dimensions; i++) {
 
   gridSquares.classList.add("boxes");
   container.appendChild(gridSquares);
+  draw.click(); // clicked by default
 
-  //one click and paint
-  gridSquares.addEventListener("mousedown", (event) => {
-    if (event.buttons == 1) {
-      makeBlack(event);
-    }
+  //DRAW Function
+  draw.addEventListener("click", () => {
+    eraser.setAttribute("style", "background: white; color: black");
+    draw.setAttribute("style", "background: black; color: white");
+
+    //one click and paint
+    gridSquares.addEventListener("mousedown", (event) => {
+      if (event.buttons == 1) {
+        makeBlack(event);
+      }
+    });
+
+    //drag click to paint
+    gridSquares.addEventListener("mouseenter", (event) => {
+      if (event.buttons == 1) {
+        makeBlack(event);
+      }
+    });
   });
 
-  //click and hold/drag mouse then paint
-  gridSquares.addEventListener("mouseenter", (event) => {
-    if (event.buttons == 1) {
-      makeBlack(event);
-    }
+  //ERASER Function
+  eraser.addEventListener("click", () => {
+    eraser.setAttribute("style", "background: black; color: white");
+    draw.setAttribute("style", "background: white; color: black");
+
+
+    gridSquares.addEventListener("mouseenter", (event) => {
+      if (event.buttons == 1) {
+        makeErase(event);
+      }
+    });
+
+    gridSquares.addEventListener("mousedown", (event) => {
+      if (event.buttons == 1) {
+        makeErase(event);
+      }
+    });
   });
+
+  clear.addEventListener("click", () => {
+    eraser.setAttribute("style", "background: white; color: black");
+    draw.setAttribute("style", "background: black; color: white");
+    makeClear(gridSquares);
+  })
 }
+
+/*
+ eraser.setAttribute("style", "background: black; color: white");
+  draw.setAttribute("style", "background: black; color: white");
+*/
