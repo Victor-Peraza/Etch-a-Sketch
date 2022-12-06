eaafbe027a0229f6ac91f7b29830afc2a4c418f5
Victor Peraza <perazav@outlook.com>
2022-12-05 22:25:15 -0800

    added pixel size changer and optimize code for effeciency and performance

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 const grid = document.querySelector("#grid");
-let squares = 16;
-const dimensions = squares * squares;
-const padding = 1;
-
 const draw = document.querySelector("#draw");
 const eraser = document.querySelector("#eraser");
 const clear = document.querySelector("#clear");
-
 const pickColor = document.querySelector("#colorPicker");
+const pickNumOfSquares = document.querySelector("#range");
+const rangeValue = document.querySelector("#range-value");
 
 const makeColor = (event) =>
   (event.target.style.backgroundColor = pickColor.value);
 const makeErase = (event) => (event.target.style.backgroundColor = "#F8F8F8");
 const makeClear = (event) => (event.style.backgroundColor = "#F8F8F8");
 
-for (let i = 0; i < dimensions; i++) {
-  const gridSquares = document.createElement("div");
+rangeValue.textContent = pickNumOfSquares.value + " x " + pickNumOfSquares.value;
 
+let currentMode = "draw";
+function setCurrentMode(mode) {
+  currentMode = mode;
+}
+
+function createGrid(squares) {
+  let dimensions = squares * squares;
+  const padding = 1;
   grid.style =
     "grid-template-columns: repeat(" +
     squares +
@@ -28,57 +32,49 @@ for (let i = 0; i < dimensions; i++) {
     padding +
     "fr);";
 
-  gridSquares.classList.add("boxes");
-  grid.appendChild(gridSquares);
-  draw.click(); // clicked by default
+  for (let i = 0; i < dimensions; i++) {
+    const gridSquares = document.createElement("div");
+    gridSquares.classList.add("boxes"); //makes pixel not draggable as link and adds class name
+    grid.appendChild(gridSquares);
 
-  //DRAW Function
-  draw.addEventListener("click", () => {
-    eraser.setAttribute("style", "color: black");
-    draw.setAttribute("style", "background: black; color: white");
+    //one click and paint and erase
+    gridSquares.addEventListener("mousedown", toolMode);
+    //drag click to paint and erase
+    gridSquares.addEventListener("mouseenter", toolMode);
 
-    //one click and paint
-    gridSquares.addEventListener("mousedown", (event) => {
-      if (event.buttons == 1) {
-        makeColor(event);
-      }
+    clear.addEventListener("click", () => {
+      makeClear(gridSquares);
     });
-
-    //drag click to paint
-    gridSquares.addEventListener("mouseenter", (event) => {
-      if (event.buttons == 1) {
-        makeColor(event);
-      }
-    });
-  });
-
-  //ERASER Function
-  eraser.addEventListener("click", () => {
-    eraser.setAttribute("style", "background: black; color: white");
-    draw.setAttribute("style", "color: black");
-
-    gridSquares.addEventListener("mouseenter", (event) => {
-      if (event.buttons == 1) {
-        makeErase(event);
-      }
-    });
-
-    gridSquares.addEventListener("mousedown", (event) => {
-      if (event.buttons == 1) {
-        makeErase(event);
-      }
-    });
-  });
-
-  //CLEAR
-  clear.addEventListener("click", () => {
-    eraser.setAttribute("style", "; color: black");
-    draw.setAttribute("style", "background: black; color: white");
-    makeClear(gridSquares);
-  });
+  }
 }
 
-/*
- eraser.setAttribute("style", "background: black; color: white");
-  draw.setAttribute("style", "background: black; color: white");
-*/
+function toolMode(e) {
+  if (e.buttons == 1 && currentMode == "draw") {
+    makeColor(e);
+  } else if (e.buttons == 1 && currentMode == "erase") {
+    makeErase(e);
+  }
+}
+
+function pixelSize() {
+  let gridPixels = grid.querySelectorAll("div");
+  gridPixels.forEach((gridPixel) => gridPixel.remove("div"));
+  rangeValue.textContent = pickNumOfSquares.value + " x " + pickNumOfSquares.value;
+  createGrid(pickNumOfSquares.value);
+}
+
+draw.onclick = () => {
+  setCurrentMode("draw");
+  draw.classList.add("button-active");
+  eraser.classList.remove("button-active");
+};
+
+eraser.onclick = () => {
+  setCurrentMode("erase");
+  draw.classList.remove("button-active");
+  eraser.classList.add("button-active");
+};
+
+createGrid(pickNumOfSquares.value);
+draw.click(); //draw clicked by default
+pickNumOfSquares.addEventListener("mouseup", pixelSize);
